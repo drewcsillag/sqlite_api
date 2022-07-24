dcfbe6b9ffef268a63464a612a15699e47975c6a
Drew Csillag <drew@thecsillags.com>
2022-07-24 19:02:22 -0400

    80% writefiled

--- api.sql
+++ api.sql
@@ -3,8 +3,10 @@
 
 .mode list
 
-.once .sqlite_temp/docalls.out
-SELECT 
+SELECT a FROM ( -- to inhibit the output of writefile
+SELECT writefile('.sqlite_temp/docalls.out', group_concat(lines.block, char(10))) a -- group_concat to glue
+FROM (
+  SELECT 0 as key,
 "
 ----- Move first record from api.call to api._call
 INSERT INTO api._call
@@ -12,28 +14,38 @@ WITH config AS (
     SELECT COALESCE( (SELECT path FROM api.config), './') AS path
 )
   SELECT func, arg1, arg2, arg3, arg4 FROM api.call, (SELECT MIN(rowid) AS m FROM api.call) fmin WHERE fmin.m = api.call.rowid;
-.once .sqlite_temp/docall1.out
-SELECT '.read ' || (SELECT COALESCE( (SELECT path FROM api.config), './') AS path) || 'call_' || func || '.sql' from api._call;
+SELECT a FROM ( -- to inhibit the output of writefile
+SELECT writefile('.sqlite_temp/docall1.out', line) a FROM (
+  SELECT '.read ' || (SELECT COALESCE( (SELECT path FROM api.config), './') AS path) || 'call_' || func || '.sql' AS line from api._call
+))
+WHERE a = 0;
 .read .sqlite_temp/docall1.out
 DELETE from api._call;
 DELETE from api.call WHERE rowid = (select min(rowid) from api.call);
 ----- End of instruction
-"
-FROM api.call;
+" as block 
+FROM api.call
+) lines
+GROUP BY key
+) WHERE a = 0;
 .read .sqlite_temp/docalls.out
 
 
-.once .sqlite_temp/docall1.out
-WITH config AS (
-    SELECT COALESCE( (SELECT path FROM api.config), './') AS path
-)
-SELECT '
------ Read file for the function
-.read ' || (SELECT COALESCE( (SELECT path FROM api.config), './') AS path) || 'call_' || func || '.sql
------ End of function .read
-' from api._call;
-.read .sqlite_temp/docall1.out
-DELETE from api._call;
+-- .once .sqlite_temp/docall1.out
+
+-- SELECT a FROM ( -- to inhibit the output of writefile
+-- SELECT writefile('.sqlite_temp/docall1.out', line) a FROM (
+-- WITH config AS (
+--     SELECT COALESCE( (SELECT path FROM api.config), './') AS path
+-- )
+-- SELECT '
+-- ----- Read file for the function
+-- .read ' || (SELECT COALESCE( (SELECT path FROM api.config), './') AS path) || 'call_' || func || '.sql
+-- ----- End of function .read
+-- ' as line from api._call)) WHERE a = 0;
+
+-- .read .sqlite_temp/docall1.out
+-- DELETE from api._call;
 
 .once .sqlite_temp/docall0-1.out
 WITH inds AS (
--- call_json_explode_arr.sql
+++ call_json_explode_arr.sql
@@ -4,7 +4,11 @@
 
 -- generate
 -- CREATE TABLE THE_NEW_TABLE AS SELECT je.value FROM json_each(z.THE_COLUMN) je, THE_TABLE z;
-.once .sqlite_temp/docall2.out
+
+-- .once .sqlite_temp/docall2.out
+SELECT a FROM ( -- to inhibit the output of writefile
+SELECT writefile('.sqlite_temp/docall2.out', line) a FROM (
+
 WITH THE_CALL AS (
     SELECT arg1 AS THE_TABLE, arg2 AS THE_COLUMN, arg3 AS THE_NEW_TABLE
     FROM api._call
@@ -13,6 +17,8 @@ SELECT "CREATE TABLE `" || THE_NEW_TABLE || "` AS
     SELECT z.rowid as src_rowid, je.value
     FROM json_each(z.`" || THE_COLUMN || "`) je, `" || THE_TABLE || "` z 
     WHERE json_valid(z.`" || THE_COLUMN || "`)
-      AND 'array' = json_type(z.`" || THE_COLUMN || "`);"
-FROM THE_CALL;
+      AND 'array' = json_type(z.`" || THE_COLUMN || "`);" as line
+FROM THE_CALL
+)) WHERE a = 0
+;
 .read .sqlite_temp/docall2.out
